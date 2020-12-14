a53bb851ace687c7b3faf248312b47f650d68163
amr17-ca <amroali.ca@gmail.com>
2020-12-14 16:26:18 +0000

    node js docker webpack es6 starter

---
+++ README.md
@@ -0,0 +1,5 @@
+git add . 
+git commit -m "node js docker webpack es6 starter"
+git branch -M main
+git remote add origin https://github.com/amr17-ca/nodejs-docker-webpack-es6-starter-.git
+git push -u origin main
---
+++ backend/data.js
@@ -0,0 +1,36 @@
+module.exports = {
+  products: [
+    {
+      _id: '1',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae sagittis lorem, nec tempor ipsum. Interdum et malesuada fames ac',
+      category: 'Shirts',
+      image: '/images/product-1.jpg',
+      price: 40,
+      brand: 'Pokemon',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae sagittis lorem, nec tempor ipsum. Interdum et malesuada fames ac',
+      category: 'Shirts',
+      image: '/images/product-2.jpg',
+      price: 70,
+      brand: 'Pokemon',
+      rating: 5,
+      numReviews: 5,
+    },
+    {
+      _id: '3',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae sagittis lorem, nec tempor ipsum. Interdum et malesuada fames ac',
+      category: 'Shirts',
+      image: '/images/product-3.jpg',
+      price: 59,
+      brand: 'Pokemon',
+      rating: 5,
+      numReviews: 55,
+    },
+   
+  ],
+};
